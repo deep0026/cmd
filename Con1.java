c8f9e5bdc6b897dc32f101f1a695275d7cfd1bc3
deep0026 <njayadeep69@gmail.com>
2023-03-07 19:04:18 +0530

    new project

---
+++ Con1.java
@@ -0,0 +1,8 @@
+class Con1{
+Con1(){
+System.out.println("hello");
+}
+static public void main(String args[]){
+Con1 al=new Con1();
+}
+}
---
+++ Try.java
@@ -0,0 +1,11 @@
+class Try{
+static public void main(String args[]){
+System.out.println("hello india");
+for(int i=1;i<=10;i+=2){
+System.out.print(i);
+}
+for(int i=2;i<=10;i+=2){
+System.out.println(i);
+}
+}
+}
